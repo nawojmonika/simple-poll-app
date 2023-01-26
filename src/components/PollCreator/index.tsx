efc526b6c71322d689e220e5ca072c08ba07d826
nawojmonika <nawojmonika@gmail.com>
2023-01-26 12:46:46 +0100

    added uuid for gernerating unique ids

--- src/components/PollCreator/index.tsx
+++ src/components/PollCreator/index.tsx
@@ -1,25 +1,32 @@
 import { useState } from 'react';
 import { TextInput, TextInputProps } from '../TextInput';
 import './PollCreator.css';
+import { v4 as uuidv4 } from 'uuid';
 
-const defaultOptions: TextInputProps[] = [
+type Option = TextInputProps & {
+    id: string;
+}
+
+const defaultOptions: Option[] = [
     {
+        id: uuidv4(),
         placeholder: 'first option',
     },
     {
+        id: uuidv4(),
         placeholder: 'second option',
     }
 ]
 
 export const PollCreator = () => {
-    const [options, setOptions] = useState<TextInputProps[]>(defaultOptions);
+    const [options, setOptions] = useState<Option[]>(defaultOptions);
     return (
         <div>
             <TextInput placeholder='What is the question?' />
             <div className='optionList'>
                 {options.map((option) => {
                     return (
-                        <div className='option'>
+                        <div key={option.id} className='option'>
                             <TextInput {...option} />
                             <button>X</button>
                         </div>);
