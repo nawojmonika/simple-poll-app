73b0b1babbba656b12fcccc3e4a40e5c36cb86b3
nawojmonika <nawojmonika@gmail.com>
2023-01-27 18:52:20 +0100

    added bars to chart

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,6 +1,5 @@
 
 import * as d3 from 'd3';
-import { drag, scaleBand, scaleLinear } from 'd3';
 import { useEffect, useRef } from 'react';
 import { Option } from '../OptionsContext';
 
@@ -13,37 +12,69 @@ type Props = {
 };
 
 export const Chart = ({ data, caption, total }: Props): JSX.Element => {
-    const svgElement = useRef<SVGSVGElement>(null);
+    const chartContainer = useRef<HTMLDivElement>(null);
     const margin = { top: 20, right: 20, bottom: 40, left: 60 },
         width = 460 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+        height = 400 - margin.top - margin.bottom,
+        xRange = [margin.left, width - margin.right],
+        yRange = [height - margin.bottom, margin.top],
+        xPadding = 0.1;
 
     useEffect(() => {
-        const svg = d3.select(svgElement.current).append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform",
-                "translate(" + margin.left + "," + margin.top + ")");;
-        const scaleX = scaleBand()
-            .domain(data.map(({ value }) => value))
-            .range([0, width]);
-        const scaleY = scaleLinear()
-            .domain([0, Math.max(total, 10)])
-            .range([height, 0]);
+        const X = d3.map(data, d => d.value),
+            Y = d3.map(data, d => d.votes),
+            xDomain = new d3.InternSet(X),
+            yDomain = [0, Math.max(total, 10)],
+            I = d3.range(X.length).filter(i => xDomain.has(X[i]));
 
-        svg.append("g").attr('transform', `translate(0, ${height})`).call(d3.axisBottom(scaleX));
-        svg.append("g").call(d3.axisLeft(scaleY));
+        const xScale = d3.scaleBand(xDomain, xRange).padding(xPadding);
+        const yScale = d3.scaleLinear(yDomain, yRange);
+        const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
+        const yAxis = d3.axisLeft(yScale).ticks(height / 40);
+
+        const svg = d3.select(chartContainer.current).append('svg')
+            .attr("width", width)
+            .attr("height", height)
+            .attr("viewBox", [0, 0, width, height])
+            .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+        svg.append("g")
+            .attr("transform", `translate(${margin.left},0)`)
+            .call(yAxis)
+            .call(g => g.select(".domain").remove())
+            .call(g => g.selectAll(".tick line").clone()
+                .attr("x2", width - margin.left - margin.right)
+                .attr("stroke-opacity", 0.1))
+            .call(g => g.append("text")
+                .attr("x", -margin.left)
+                .attr("y", 10)
+                .attr("fill", "currentColor")
+                .attr("text-anchor", "start"));
+
+        const bar = svg.append("g")
+            .attr("fill", 'red')
+            .selectAll("rect")
+            .data(I)
+            .join("rect")
+            .attr("x", i => xScale(X[i]) || '')
+            .attr("y", i => yScale(Y[i]))
+            .attr("height", i => yScale(0) - yScale(Y[i]))
+            .attr("width", xScale.bandwidth());
+
+        bar.append("title")
+            .text(caption);
+
+        svg.append("g")
+            .attr("transform", `translate(0,${height - margin.bottom})`)
+            .call(xAxis);
+
+        return () => {
+            d3.selectAll('svg').remove();
+        };
 
     }, []);
 
 
     return (
-        <svg
-            ref={svgElement}
-            width={width + margin.left + margin.right}
-            height={height + margin.top + margin.bottom}
-        >
-        </svg >
+        <div ref={chartContainer} />
     );
-}
+};
