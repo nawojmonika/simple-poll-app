753f6b906682ce31d33433e78c7253a0e41a4e46
nawojmonika <nawojmonika@gmail.com>
2023-01-28 01:54:48 +0100

    made transitions for x axis

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -12,6 +12,9 @@ type Props = {
 
 export const Chart = ({ data, caption }: Props): JSX.Element => {
     const chartContainer = useRef<SVGSVGElement>(null);
+    const svg = useRef<d3.Selection<SVGSVGElement | null, unknown, null, undefined>>();
+    const xAxis = useRef<d3.Selection<SVGGElement, unknown, null, undefined>>();
+    const xScale = useRef<d3.ScaleBand<string>>();
     const margin = { top: 20, right: 20, bottom: 40, left: 60 },
         width = 460 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom,
@@ -20,17 +23,25 @@ export const Chart = ({ data, caption }: Props): JSX.Element => {
         xPadding = 0.1;
 
     useEffect(() => {
+        const x = d3.map(data, d => d.value);
+        xScale.current?.domain(x);
+        xAxis.current?.transition().duration(1000).call(d3.axisBottom(xScale.current!!));
+    }, [data])
+
+    useEffect(() => {
         const X = d3.map(data, d => d.value),
             Y = d3.map(data, d => d.votes),
-            xDomain = new d3.InternSet(X),
             yDomain = [0, Math.max(d3.max(Y) || 0 + 5, 10)],
-            xScale = d3.scaleBand(xDomain, xRange).padding(xPadding),
             yScale = d3.scaleLinear(yDomain, yRange),
-            xAxis = d3.axisBottom(xScale).tickSizeOuter(0),
             yAxis = d3.axisLeft(yScale).ticks(height / 40);
 
-        const svg = d3.select(chartContainer.current);
-        svg.append('g')
+        svg.current = d3.select(chartContainer.current);
+        xScale.current = d3.scaleBand(X, xRange).padding(xPadding);
+        xAxis.current = svg.current.append('g')
+            .attr('transform', `translate(0,${height - margin.bottom})`)
+            .call(d3.axisBottom(xScale.current).tickSizeOuter(0));
+
+        svg.current?.append('g')
             .attr('transform', `translate(${margin.left},0)`)
             .call(yAxis)
             .call(g => g.select('.domain').remove())
@@ -40,32 +51,27 @@ export const Chart = ({ data, caption }: Props): JSX.Element => {
             .call(g => g.append('text')
                 .attr('x', -margin.left)
                 .attr('y', 10)
-                .attr('fill', 'currentColor')
                 .attr('text-anchor', 'start'));
 
-        svg.append('g')
+        svg.current?.append('g')
             .selectAll('rect')
             .data(data)
             .join('rect')
             .attr('fill', (_, i) => d3.schemeCategory10[i])
-            .attr('x', d => (xScale(d.value) || 0))
+            .attr('x', d => (xScale.current?.(d.value) || 0))
             .attr('y', d => yScale(d.votes))
             .attr('height', d => yScale(0) - yScale(d.votes))
-            .attr('width', xScale.bandwidth());
+            .attr('width', xScale.current?.bandwidth());
 
-        svg.append("g")
+        svg.current?.append("g")
             .selectAll("label")
             .data(data)
             .join('text')
             .text(d => d.votes)
-            .attr('x', d => (xScale(d.value) || 0) + (xScale.bandwidth() / 2) - 5)
+            .attr('x', d => (xScale.current?.(d.value) || 0) + ((xScale.current?.bandwidth() || 0) / 2) - 5)
             .attr('y', d => yScale(d.votes) - 5);
 
-        svg.append('g')
-            .attr('transform', `translate(0,${height - margin.bottom})`)
-            .call(xAxis);
-
-        svg.append('text')
+        svg.current?.append('text')
             .attr('y', 15)
             .attr('x', '50%')
             .attr('text-anchor', 'middle')
